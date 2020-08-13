c6c6fc3d3ce66a3cea73e85bedce70120a657823
CRUNZEX <crunzex@gmail.com>
2020-08-13 13:50:31 +0700

    Challenge_loop_if_basicmath

--- Challenge_loop_if_basicmath/Source.cpp
+++ Challenge_loop_if_basicmath/Source.cpp
@@ -11,7 +11,7 @@ int main() {
 	float sum = 0, sumPow = 0;
 	scanf_s("%d %d", &num1, &num2);
 
-	if (num1 >= num2)
+	if (num1 > num2)
 	{
 		for (int i = num1; i >= num2; i--)
 		{
@@ -22,6 +22,14 @@ int main() {
 
 		printf("\nAverage = %.1f", sum / (num1 - num2 + 1));
 		c = num1 - num2 + 1;
+		printf("\nSD = %.2f", calculateSD(num1, num2, sum, sumPow, c));
+	}
+
+	else if (num1 == num2)
+	{
+		printf("%d", num1);
+		printf("\nAverage = %d", num1);
+		printf("\nSD = 0");
 	}
 
 	else
@@ -35,8 +43,8 @@ int main() {
 
 		printf("\nAverage = %.1f", sum / (num2 - num1 + 1));
 		c = num2 - num1 + 1;
+		printf("\nSD = %.2f", calculateSD(num1, num2, sum, sumPow, c));
 	}
 
-	printf("\nSD = %.2f", calculateSD(num1, num2, sum, sumPow, c));
 	return 0;
 }
